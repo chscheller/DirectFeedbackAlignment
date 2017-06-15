d39d0b4250fec4f48ebcd6782311581615ad49d4
Christian Scheller <ch.scheller@hotmail.com>
2017-06-15 10:02:38 +0200

    removed setup from utils folder

--- network/utils/im2col_cython_setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-from Cython.Build import cythonize
-import numpy as np
-
-setup(
-  name='im2col',
-  ext_modules=cythonize("im2col_cython.pyx"),
-  include_dirs=[np.get_include()]
-)
